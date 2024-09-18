e6434fed3501158465d32eb5fffcf0338058b5f7
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-09-18 16:22:04 +0200

    [UPD] defined Category class

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php 
+// definisco la classe delle categorie 
+class Category {
+    public $name;
+    public $icon;
+    public $url;
+    // construct 
+    public function __construct($name, $icon, $url) {
+        $this->name = $name;
+        $this->icon = $icon;
+        $this->url = $url;
+    }
+
+    
+}
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <title>Pet Shop</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
